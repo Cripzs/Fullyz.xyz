cb2891233e3b2da8dd7dc41f442defee640e0407
Cripzs <110641999+Cripzs@users.noreply.github.com>
2023-08-20 00:31:29 +0200

    Update Fix.lua

--- Fix.lua
+++ Fix.lua
@@ -1,7 +1,7 @@
 local FullyzFolder = "Fullyz.xyz"
 
-if isfolder(logFolderPath) then
-    delfolder(logFolderPath)
+if isfolder(FullyzFolder) then
+    delfolder(FullyzFolder)
     print('Deleted folder called "Fullyz.xyz".')
 else
     print('Folder called "Fullyz.xyz" not found')
