a76051bf1b47b633fa63977fb7afb2bf3877c8bc
Cripzs <110641999+Cripzs@users.noreply.github.com>
2024-11-10 10:51:14 +0100

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,2 +1,2 @@
 setclipboard("https://discord.gg/ABP6aXf5PZ")
-game:GetService("Players").LocalPlayer:Kick("[Resumed Development!] The Fullyz.xyz development has been resumed, copied the discord invite into your clipboard!")
+game:GetService("Players").LocalPlayer:Kick("The Fullyz.xyz development has been resumed, copied the discord invite into your clipboard!")
