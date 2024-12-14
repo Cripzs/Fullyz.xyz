e83bf7b2fb755853bea48af9dfe5adcbf007b2d7
h-cropw <110641999+h-cropw@users.noreply.github.com>
2024-12-14 11:24:13 +0100

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,5 +1,4 @@
-print("Authenficated")
-print("The Fullyz.xyz development has been resumed!")
+print("The Fullyz.xyz development has been resumed! 14/12/2024")
 print("https://discord.gg/ABP6aXf5PZ")
 --setclipboard("https://discord.gg/ABP6aXf5PZ")
 --game:GetService("Players").LocalPlayer:Kick("The Fullyz.xyz development has been resumed, copied the discord invite into your clipboard!")
